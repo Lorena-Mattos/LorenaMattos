90ac5c18528647a40ff299477863cee72f9eaea9
Lorena Mattos <91705201+Lorena-Mattos@users.noreply.github.com>
2021-10-03 18:19:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Treinando HTML</title>
+    <link rel="stylesheet" href="estilo.css" type="text/css">
+  </head>
+  <body class="tela-azul">
+    <b> <u>ConstruDelas treinamento</u></b> 
+    <br> Aprendendo HTML </br>
+  </body>
+</html>
