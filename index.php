14f2be50aa865fea4bdac0efb229a9daff9d7807
Lorena Mattos <91705201+Lorena-Mattos@users.noreply.github.com>
2021-10-03 18:23:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     <link rel="stylesheet" href="estilo.css" type="text/css">
   </head>
   <body class="tela-azul">
+    .tela-azul{
+  background-color: rgb(4, 0, 5);
+  color:white;
+  text-align: center;
+  font-size: 50px;
+}
     <b> <u>ConstruDelas treinamento</u></b> 
     <br> Aprendendo HTML </br>
   </body>
